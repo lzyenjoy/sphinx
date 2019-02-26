01e05e93d728072eea57414c5a89244b92ccc4a1
lzy <lzy_email@sina.cn>
2019-02-26 21:46:21 +0800

    不知道

--- search.php
+++ search.php
@@ -3,7 +3,7 @@ require_once 'sphinxapi.php';
 use Sphinx\SphinxClient;
 $s= new SphinxClient();
 $s->setServer('localhost',9312);
-$keyword = "短袖";//要搜索的关键字
+$keyword = "回忆";//要搜索的关键字
 $index = 'goods';//索引名称
 //查询出关键字所在的主键id
 $s->_limit = 2000;
@@ -18,7 +18,7 @@ if (isset($res['matches'])){
 }
 $mysql_conf = array(
     'host'    => '127.0.0.1:3306',
-    'db'      => 'zhlx',
+    'db'      => 'sphinx',
     'db_user' => 'root',
     'db_pwd'  => '',
 );
@@ -31,7 +31,7 @@ $select_db = $mysqli->select_db($mysql_conf['db']);
 if (!$select_db) {
     die("could not connect to the db:\n" .  $mysqli->error);
 }
-$sql = "select * from good where id in ($ids)";
+$sql = "select * from documents where id in ($ids)";
 $res = $mysqli->query($sql);
 if (!$res) {
     die("sql error:\n" . $mysqli->error);
